7d44130c4ea5af5be12e6567b603365fd01e1bf2
BjoernS <bjoerns@lucanet.com>
2020-09-03 17:23:51 +0200

    - adding a new case for Unboxing (adding two different Integer Objects) - improved the sum of two primitive ints by making it two different int values

--- src/main/java/net/steel/performance/autoboxing/AutoBoxingBenchmark.java
+++ src/main/java/net/steel/performance/autoboxing/AutoBoxingBenchmark.java
@@ -12,41 +12,35 @@ import java.util.concurrent.TimeUnit;
 
 public class AutoBoxingBenchmark {
 
-    private static final int ITERATIONS_WARMUP  = 2;
-    private static final int ITERATIONS_MEASURE = 3;
+    private static final int ITERATIONS_WARMUP  = 3;
+    private static final int ITERATIONS_MEASURE = 10;
 
     private static final int LOOP_SIZE = 50;
 
     @State(Scope.Benchmark)
     public static class MyState {
 
-        private int randomInt;
-        private Integer randomInteger;
+        int[] randomInts = new int[LOOP_SIZE];
+        Integer[] randomIntegers = new Integer[LOOP_SIZE];
+        Integer[] randomIntegers2 = new Integer[LOOP_SIZE];
         Random random = new Random();
+        private int randomInt = random.nextInt() + 500;
+        private int randomInt2 = random.nextInt() + 500;
+        private Integer randomInteger = Integer.valueOf(randomInt) + 500;
+        private Integer randomInteger2 = Integer.valueOf(randomInt) + 500;
 
-        private Long totalLongObject;
-        private long totalLongPrim;
+        private Long totalLongObject = 245633L;
+        private long totalLongPrim = 27L;
 
-        @Setup(Level.Invocation)
+        @Setup(Level.Trial)
         public void setup() {
-
-            totalLongObject = 23L;
-            totalLongPrim = 27;
-
-            randomInt = random.nextInt();
-            randomInteger = Integer.valueOf(randomInt);
+            for (int i = 0; i < LOOP_SIZE; i++) {
+                randomInts[i] = random.nextInt() + 500;
+                randomIntegers[i] = Integer.valueOf(randomInts[i]);
+                randomIntegers2[i] = Integer.valueOf(randomInts[i] + 7);
+            }
         }
-    }
 
-
-    @Benchmark
-    @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    @Fork(1)
-    @Warmup(iterations = ITERATIONS_WARMUP)
-    @Measurement(iterations = ITERATIONS_MEASURE)
-    @BenchmarkMode(Mode.AverageTime)
-    public Long sumIntObject(AutoBoxingBenchmark.MyState myState) {
-        return myState.totalLongObject + myState.randomInt;
     }
 
     @Benchmark
@@ -55,8 +49,48 @@ public class AutoBoxingBenchmark {
     @Warmup(iterations = ITERATIONS_WARMUP)
     @Measurement(iterations = ITERATIONS_MEASURE)
     @BenchmarkMode(Mode.AverageTime)
-    public long sumIntPrimitive(AutoBoxingBenchmark.MyState myState) {
-        return myState.totalLongPrim + myState.randomInt;
+    public int remainderIntPrimitive(AutoBoxingBenchmark.MyState myState) {
+        return sumEvenPrimitive(myState.randomInts);
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Fork(1)
+    @Warmup(iterations = ITERATIONS_WARMUP)
+    @Measurement(iterations = ITERATIONS_MEASURE)
+    @BenchmarkMode(Mode.AverageTime)
+    public Integer remainderIntObject(AutoBoxingBenchmark.MyState myState) {
+        return sumEvenObject(myState.randomIntegers);
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Fork(1)
+    @Warmup(iterations = ITERATIONS_WARMUP)
+    @Measurement(iterations = ITERATIONS_MEASURE)
+    @BenchmarkMode(Mode.AverageTime)
+    public Integer sum2Integers(AutoBoxingBenchmark.MyState myState) {
+        return myState.randomInteger + myState.randomInteger2;
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Fork(1)
+    @Warmup(iterations = ITERATIONS_WARMUP)
+    @Measurement(iterations = ITERATIONS_MEASURE)
+    @BenchmarkMode(Mode.AverageTime)
+    public Integer sumIntInteger(AutoBoxingBenchmark.MyState myState) {
+        return myState.randomInteger + myState.randomInt;
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Fork(1)
+    @Warmup(iterations = ITERATIONS_WARMUP)
+    @Measurement(iterations = ITERATIONS_MEASURE)
+    @BenchmarkMode(Mode.AverageTime)
+    public int sum2IntPrimitives(AutoBoxingBenchmark.MyState myState) {
+        return myState.randomInt + myState.randomInt2;
     }
 
     @Benchmark
@@ -101,5 +135,24 @@ public class AutoBoxingBenchmark {
         return li;
     }
 
+    private int sumEvenPrimitive(int[] primitives) {
+        int sum = 0;
+        for (int i = 0; i < primitives.length; i++) {
+            if (primitives[i] % 2 == 0) {
+                sum += primitives[i];
+            }
+        }
+        return sum;
+    }
+
+    private Integer sumEvenObject(Integer[] primitives) {
+        Integer sum = 0;
+        for (int i = 0; i < primitives.length; i++) {
+            if (primitives[i] % 2 == 0) {
+                sum += primitives[i];
+            }
+        }
+        return sum;
+    }
 
 }
